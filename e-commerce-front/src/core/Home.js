ff6ecb5589326f3d78361cf8d25655cda7aa9815
Fremen1990 <fremen1990@gmail.com>
2021-05-31 01:54:57 +0200

    new structure back-end / front-end

--- e-commerce-front/src/core/Home.js
+++ e-commerce-front/src/core/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Layout from "./Layout";
+import { API } from "../config";
 
 const Home = () => (
   <Layout
     title="Home Page"
     description="MERN Node React Full Stack E-Commerce App"
   >
-    {process.env.REACT_APP_API_URL}
+    {API}
     ....
   </Layout>
 );
