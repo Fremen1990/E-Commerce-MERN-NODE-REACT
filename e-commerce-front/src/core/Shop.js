b38fc38fd68fd4d9b39da28b90a54c1257bee06a
Fremen1990 <fremen1990@gmail.com>
2021-06-07 01:41:25 +0200

    search filter by category and price feature part 1

--- e-commerce-front/src/core/Shop.js
+++ e-commerce-front/src/core/Shop.js
@@ -1,8 +1,42 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
 import Card from "./Card";
+import { getCategories } from "./apiCore";
+import Checkbox from "./Checkbox";
 
 const Shop = () => {
+  const [myFilters, setMyFilters] = useState({
+    filters: {
+      category: [],
+      price: [],
+    },
+  });
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState(false);
+
+  // Load categories and set form data
+  const init = () => {
+    getCategories().then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setCategories(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const handleFilters = (filters, filterBy) => {
+    // console.log("SHOP", filters, filterBy);
+
+    const newFilters = { ...myFilters };
+    newFilters.filters[filterBy] = filters;
+    setMyFilters(newFilters);
+  };
+
   return (
     <Layout
       title="Shop Page"
@@ -10,8 +44,16 @@ const Shop = () => {
       className="container-fluid"
     >
       <divv className="row">
-        <div className="col-4">Left sidebar</div>
-        <div className="col-8">Right sidebar</div>
+        <div className="col-4">
+          <h4>Filter by categories</h4>
+          <ul>
+            <Checkbox
+              categories={categories}
+              handleFilters={(filters) => handleFilters(filters, "category")}
+            />
+          </ul>
+        </div>
+        <div className="col-8">{JSON.stringify(myFilters)}</div>
       </divv>
     </Layout>
   );
