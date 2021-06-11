f8380c48c36f251b2ae1d2e669beb32c272cc64e
Fremen1990 <fremen1990@gmail.com>
2021-06-11 15:47:37 +0200

    Cart CRUD with local storage

--- e-commerce-front/src/core/Menu.js
+++ e-commerce-front/src/core/Menu.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
 import { signOut, isAuthenticated } from "../auth/index";
+import { itemTotal } from "./cartHelpers";
 
 const isActive = (history, path) => {
   if (history.location.pathname === path) {
@@ -26,6 +27,19 @@ const Menu = ({ history }) => (
           to="/shop"
         >
           Shop
+        </Link>
+      </li>
+
+      <li className="nav-item">
+        <Link
+          className="nav-link"
+          style={isActive(history, "/cart")}
+          to="/cart"
+        >
+          Cart
+          <sup>
+            <small className="cart-badge">{itemTotal()}</small>
+          </sup>
         </Link>
       </li>
 
